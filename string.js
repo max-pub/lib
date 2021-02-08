b5db853f1eef3034950d126d1dc6c9aef45ccd60
Max Fechner <max.fechner@gmail.com>
2021-02-08 23:39:06 +0100

    - use getter instead of function call

--- string.js
+++ string.js
@@ -4,7 +4,9 @@ Object.defineProperties(String.prototype, {
 	toTitleCase: { value: function () { return string.toTitleCase(this) } },
 	toCamelCase: { value: function () { return string.toCamelCase(this) } },
 	removeTags: { value: function () { return string.removeTags(this) } },
-	blocks: { value: function () { return string.blocks(this) } },
-	lines: { value: function () { return string.lines(this) } },
 	findGroups: { value: function (regex) { return string.findGroups(this, regex) } },
+
+	blocks: { get: function () { return string.blocks(this) } },
+	lines: { get: function () { return string.lines(this) } },
+	camelCase: { get: function () { return string.toCamelCase(this) } },
 });
